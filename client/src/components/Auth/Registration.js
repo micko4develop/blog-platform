1f6f2884bea363eca0b92b19e17d1d98ab6e1c2f
Milovan Markovic <milovan0807@gmail.com>
2024-04-13 14:00:59 +0200

    minor fix

--- client/src/components/Auth/Registration.js
+++ client/src/components/Auth/Registration.js
@@ -1,54 +1,54 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Registration = ({ onRegistration }) => {
+  const navigate = useNavigate();
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleRegistration = async () => { 
-    
-    try { 
-        
-        const response = await fetch("http://127.0.0.1:3001/api/users", { 
-            method: "POST", headers: { "Content-Type": "application/json", }, 
-            body: JSON.stringify({ firstName, lastName, email, password, }), 
-        });
-
- 
-  if (response.ok) {
-    // Registration successful, redirect or display a success message
-
-    await fetch("http://127.0.0.1:3001/api/auth", {
+  const handleRegistration = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:3001/api/users", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-      })
-        .then(response => response.json())
-        .then(data => {
-          if (data.token) {
-            // Call onLogin callback to set isLoggedIn to true in App.js
-            onRegistration();
-            navigator("/")
-          } else {
-            alert("Invalid credentials. Please try again.");
-          }
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ firstName, lastName, email, password }),
+      });
+
+      if (response.ok) {
+        await fetch("http://127.0.0.1:3001/api/auth", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
         })
-        .catch(error => {
-          console.error("Error:", error);
-          alert("An error occurred. Please try again.");
-        });
-  } else {
-    // Handle registration error, display an error message
-    alert("Registration failed");
-  }
-} catch (error) {
-  // Handle any network or server error
-  alert("An error occurred:", error);
-}
-};
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.token) {
+              // Navigate to root page before calling onRegistration()
+              navigate('/');
+              // Call onRegistration callback to set isLoggedIn to true in App.js
+              onRegistration();
+            } else {
+              alert("Invalid credentials. Please try again.");
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+          });
+      } else {
+        // Handle registration error, display an error message
+        alert("Registration failed");
+      }
+    } catch (error) {
+      // Handle any network or server error
+      alert("An error occurred:", error);
+    }
+  };
 
   return (
     <div>
